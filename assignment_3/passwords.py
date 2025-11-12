c7757fedddcc6825af9e5416314eae77f0858416
Jason Holt <jason@jasonholtdigital.com>
2025-11-12 14:28:21 +0100

    refactor: simplify password generation logic and remove debug code
    - Removed unnecessary early return condition that complicated the flow
    - Cleaned up test print statements and commented-out input code

--- assignment_3/passwords.py
+++ assignment_3/passwords.py
@@ -6,14 +6,4 @@ def easy_password(length: int = 1):
         text = get_random_text()
         if len(password + text) <= length and length - len(password + text) != 1:
             password += text
-        elif len(password) + 1 >= length:
-            return password
     return password
-
-print(easy_password(10))
-print(easy_password(10))
-print(easy_password(15))
-print(easy_password(2))
-
-# pw_length = int(input("Enter password length: "))
-# print(easy_password(pw_length))
