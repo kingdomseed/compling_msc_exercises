68e50149cf43bbd7110ec6ee97a6f022bdfc754b
Jason Holt <jason@jasonholtdigital.com>
2025-11-19 11:05:41 +0100

    Implement list utility functions and add division operator testing examples
    - Added math module import for product calculation
    - Implemented middle function to return element at center index using integer division
    - Implemented product function using math.prod for list multiplication
    - Implemented mean function calculating sum divided by length
    - Implemented even_sum function using slice notation to sum elements at even indices
    - Added testing_ideas.py with division operator examples comparing / and // behavior

--- assignment_4/list_utils.py
+++ assignment_4/list_utils.py
@@ -1,20 +1,20 @@
-
+import math
 # Returns the last element of list l
 def last(l: list):
     return l[-1]
 
-# Returns the middle element of list l
+# Returns the middle element of list l with an odd number of elements
 def middle(l: list):
-    return
+    return l[len(l) // 2]
 
 # Returns the product of a list of numbers
 def product(l: list[int]):
-    return
+    return math.prod(l)
 
 # Returns the mean (average) of a list of numbers
 def mean(l: list[int]):
-    return
+    return sum(l) / len(l)
 
 # returns the sum of the elements of a list with even indices
 def even_sum(l: list[int]):
-    return
+    return sum(l[::2])
---
+++ assignment_4/testing_ideas.py
@@ -0,0 +1,5 @@
+# basic division print test with /
+print (7 / 2)
+
+# basic division print test with //
+print (-7 // 2)
