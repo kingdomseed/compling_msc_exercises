4b378b420bf6d3d62b3a6b677bff5921b06fa2d6
Jason Holt <jason@jasonholtdigital.com>
2026-01-18 21:00:16 +0100

    feat: Add launch configuration for Python debugging and implement image loading with transformations

--- basecamp/image_class/image_loader.py
+++ basecamp/image_class/image_loader.py
@@ -1,3 +1,30 @@
+from pathlib import Path
+
 from torchvision. datasets import ImageFolder
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
+# We must convert images to tensors and apply
+# a transformation to normalize them.
+train_transforms = transforms.Compose([
+    transforms.ToTensor(),
+
+    # ensure all images are the same size
+    transforms.Resize((128, 128)),
+])
+
+# Create a dataset using ImageFolder
+script_dir = Path(__file__).resolve().parent
+dataset_train = ImageFolder(
+    str(script_dir / 'clouds_train'),
+    transform=train_transforms
+)
+
+dataloader_train = DataLoader(
+    dataset_train,
+    shuffle=True,
+    batch_size=1,
+)
+
+image, label = next(iter(dataloader_train))
+print(f'Image shape: {image.shape}, Label: {label}')
