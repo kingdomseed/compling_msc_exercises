522209d9e7608a1301075d9866b6467132a90c97
Jason Holt <jason@jasonholtdigital.com>
2026-01-22 17:10:14 +0100

    feat: Initialize happiest_per_continent dictionary and update function docstring for clarity

--- programming_cl/assignment_9/happy.py
+++ programming_cl/assignment_9/happy.py
@@ -1,10 +1,13 @@
+import csv
+
 
 def find_happiest(file):
+    happiest_per_continent = {}
     """Reads a file containing names and happiness scores, and returns the name
     with the highest happiness score.
 
     file format: csv
-    headers: number,country,score,continent
+    headers: id,country_name,score,continent_name
     example: 1,Afghanistan,3.632,Asia
 
     Args:
@@ -13,6 +16,15 @@ def find_happiest(file):
     Returns:
         dict: The most happy country per continent.
     """
-
+    with open(file, newline="", encoding='utf-8', ) as happiness_report:
+        rows = list(csv.reader(happiness_report))  # rows: list[list[str]]
+        for row in rows:
+            continent = row[3]
+            country = row[1]
+            score = float(row[2])
+            if continent not in happiest_per_continent:
+                happiest_per_continent[continent] = (country, score)
+            elif score > happiest_per_continent[continent][1]:
+                happiest_per_continent[continent] = (country, score)
 
     return happiest_per_continent
