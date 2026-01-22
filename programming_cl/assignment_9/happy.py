015c7cec46678c025a6d6bad46329a02761a9edf
Jason Holt <jason@jasonholtdigital.com>
2026-01-22 13:29:30 +0100

    feat: Implement find_happiest function to determine the happiest country per continent from a CSV file

---
+++ programming_cl/assignment_9/happy.py
@@ -0,0 +1,18 @@
+
+def find_happiest(file):
+    """Reads a file containing names and happiness scores, and returns the name
+    with the highest happiness score.
+
+    file format: csv
+    headers: number,country,score,continent
+    example: 1,Afghanistan,3.632,Asia
+
+    Args:
+        file (str): The path to the file containing names and happiness scores.
+
+    Returns:
+        dict: The most happy country per continent.
+    """
+
+
+    return happiest_per_continent
